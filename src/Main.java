2f1bd72794f4d056fd8f2bfd0676f72eb3953475
Juan <juan.flores630@gmail.com>
2014-02-19 23:09:03 -0800

    modifications to ultrasonic distance and motor rotation

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 
 
 import java.util.Random;
-
 import lejos.nxt.*;
 import lejos.robotics.navigation.DifferentialPilot;
 import lejos.util.*;
 import lejos.robotics.*;
+
 public class Main {
 	
 
@@ -19,8 +19,8 @@ public class Main {
 	
 	static long tStart;
 	static UltrasonicSensor usonic = new UltrasonicSensor(SensorPort.S1); //UltraSonic Sensor
-	static LightSensor lightRight = new LightSensor(SensorPort.S2); //Light Sensor
-	static LightSensor lightLeft = new LightSensor(SensorPort.S3); //Light Sensor
+	static LightSensor lightRight = new LightSensor(SensorPort.S3); //Light Sensor
+	static LightSensor lightLeft = new LightSensor(SensorPort.S2); //Light Sensor
 	
 	
 	//controls both wheels simutaneously
@@ -32,8 +32,8 @@ public class Main {
 	public static void main(String[] args) {	
 		
 		//Initialize Speeds
-		pilot.setTravelSpeed(8.0f);
-		pilot.setRotateSpeed(10.0f);
+		pilot.setTravelSpeed(8.0f); //juan sucks fat big hairy balls
+		pilot.setRotateSpeed(15.0f);
 		
 		//Set mode to continually check for objects
 		usonic.continuous();
@@ -47,41 +47,51 @@ public class Main {
 			long tCurr = System.currentTimeMillis();
 			delta = tCurr - tStart;
 			
-			tStart = tCurr;
+			//tStart = tCurr;
 			
 			//Move forward if nothing nearby
-			if(usonic.getDistance() > 40)
+		
+			if(usonic.getDistance() > 30)
 			{		
 				pilot.setTravelSpeed(10.0f);
 				//moveForward();
 				pilot.forward();
 				delta = 0;
+				tStart = System.currentTimeMillis();
+
 			}
 			
-			if(usonic.getDistance() <= 40){
-				pilot.quickStop();          //Stop the robot quickly
+			else if(usonic.getDistance() <= 40){
+				//pilot.quickStop();          //Stop the robot quickly
 				pilot.setTravelSpeed(5.0f);
 				pilot.forward();
 				
 			int randomNum = rand.nextInt(2);
+			
 				if(randomNum == 0)
-					pilot.rotate(15);
+					pilot.rotate(35);
 				else if(randomNum == 1)
-					pilot.rotate(-15);
+					pilot.rotate(-35);
 				
 				LCD.drawInt(randomNum, 10, 0);
 				LCD.drawString("rand", 10, 1);
+				
+				Delay.msDelay(500);
 			}
 			
-			else if(lightLeft.readValue() > 45 && usonic.getDistance() > 40){
-				pilot.rotate(10);
+			else if(lightLeft.readValue() > 45 && usonic.getDistance() < 30){
+				pilot.rotate(20);
 				LCD.drawInt(lightLeft.readValue(), 0, 0);
 				LCD.drawChar('L', 0, 1);
+				Delay.msDelay(500);
+
 			}
-			else if(lightRight.readValue() > 45 && usonic.getDistance() > 40){
-				pilot.rotate(-10);
+			else if(lightRight.readValue() > 40 && usonic.getDistance() < 30){
+				pilot.rotate(-20);
 				LCD.drawInt(lightRight.readValue(), 5, 0);
 				LCD.drawChar('R', 5, 1);
+				Delay.msDelay(500);
+
 			}
 			
 			double newDouble = (double)(delta);
@@ -93,7 +103,7 @@ public class Main {
 			if(!pilot.isMoving())
 				LCD.drawString("!moving", 0, 6);
 			
-			if(!pilot.isMoving())
+			if(!pilot.isMoving() && newDelta > 5000)
 			{
 				pilot.backward();
 				Delay.msDelay(1000);
@@ -106,6 +116,7 @@ public class Main {
 		} //Main Loop
 		
 		pilot.quickStop(); //Stop the robot quickly
+		
 	
 		
 	}
